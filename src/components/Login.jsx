229edec9556f1d17c17946371f06dbef4b4fcf68
âPratham Bindal <bindalpratham931@gmail.com>
2025-03-14 02:57:00 +0530

    fix: reset default email and password values in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,8 +7,8 @@ import { useNavigate } from "react-router";
 import backgroundImage from "../assets/bgimage.jpg";
 
 const Login = () => {
-  const [email, setEmail] = useState("pratham@gmail.com");
-  const [password, setPassword] = useState("Pratham@123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState();
   const dispatch = useDispatch();
   const navigateTo = useNavigate();
