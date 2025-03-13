560b34e4b2ef6ea470830cfb28cacc347b83bc61
âPratham Bindal <bindalpratham931@gmail.com>
2025-03-14 02:56:29 +0530

    refactor: clean up index.html and enhance Login component layout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,40 +30,52 @@ const Login = () => {
     }
   };
   return (
-    <div className="w-auto h-[100vh] bg-center bg-cover items-center justify-center flex relative" style={{ backgroundImage: `url(${backgroundImage})` }}>
-       <div className="absolute inset-0 bg-black/50"></div>
-        <div className="card card-border bg-black/80 shadow-lg shadow-black w-96">
-          <div className="card-body">
-            <h2 className="card-title">Login</h2>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Email</legend>
-              <input
-                type="text"
-                className="input"
-                placeholder="John@gmail.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </fieldset>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend">Password</legend>
-              <input
-                type="password"
-                className="input"
-                value={password}
-                placeholder="******"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </fieldset>
-            <p className="text-red-500">{error}</p>
-            <div className="card-actions justify-center my-4">
-              <button className="btn bg-[#fc6a78]" onClick={() => handleSubmit()}>
-                Login
-              </button>
-            </div>
+    <div
+      className="w-auto h-[100vh] bg-center bg-cover items-center justify-center flex relative"
+      style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="absolute inset-0 bg-black/50"></div>
+      <div className="card card-border bg-black/80 shadow-lg shadow-black w-96">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="text"
+              className="input"
+              placeholder="John@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input"
+              value={password}
+              placeholder="******"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
+          <p className="text-red-500">{error}</p>
+          <div className="card-actions justify-center my-4">
+            <button className="btn bg-[#fc6a78]" onClick={() => handleSubmit()}>
+              Login
+            </button>
+          </div>
+          <div className="flex justify-center text-center">
+            <p className="text-white/60 text-base">
+              New here?{"  "}
+              <span
+                className="font-semibold underline cursor-pointer hover:text-white/100 transition-colors"
+                onClick={() => navigateTo("/signUp")}>
+                Create an account
+              </span>
+            </p>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
