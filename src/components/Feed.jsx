e2f836c597f04dc8159a58e132470d7bd5081146
âPratham Bindal <bindalpratham931@gmail.com>
2025-03-14 03:04:53 +0530

    feat: add background image to empty feed state for improved user experience

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { addFeed } from "../utils/feedSlice";
 import FeedCard from "./FeedCard";
 import Loading from "./Loading";
+import backgroundImage from "../assets/bgimage.jpg";
 
 const Feed = () => {
   const feed = useSelector((state) => state.feed);
@@ -29,12 +30,17 @@ const Feed = () => {
   // Handle empty feed array
   if (feed.length === 0) {
     return (
-      <div className="flex flex-col justify-center items-center h-[100vh] gap-16">
-        <p className="text-center font-semibold text-2xl">
-          Looks like you've swiped through everyone!
-          <br /> Come back later for more matches.
-        </p>
-        <span className="loading loading-ring w-12"></span>
+      <div
+        className="w-auto h-[100vh] bg-center bg-cover items-center justify-center flex relative"
+        style={{ backgroundImage: `url(${backgroundImage})` }}>
+          <div className="absolute inset-0 bg-black/50"></div>
+        <div className="flex flex-col justify-center items-center h-[100vh] gap-16 absolute">
+          <p className="text-center font-semibold text-2xl">
+            Looks like you've swiped through everyone!
+            <br /> Come back later for more matches.
+          </p>
+          <span className="loading loading-ring w-12"></span>
+        </div>
       </div>
     );
   }
