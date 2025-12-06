48f973da1ba4f7d3146a5c1e4da49fcd310020b6
Pratham Bindal <76515135+pratham9467@users.noreply.github.com>
2025-12-07 03:39:55 +0530

    Change API proxy target to production URL

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://dev-tinder-azure.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
